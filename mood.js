5a36dc7ecf2ad97a8c6ea761b5afaf20af7791aa
Sandson <104211711+SandsonSoares@users.noreply.github.com>
2022-08-18 19:07:43 -0300

    Initial commit

---
+++ mood.js
@@ -0,0 +1,8 @@
+function colocar(){
+    var logar = document.getElementsByClassName("lateral")[0]
+    logar.style.marginLeft = "0"
+}
+function remover(){
+    var logar = document.getElementsByClassName("lateral")[0]
+    logar.style.marginLeft = "100%"    
+}
